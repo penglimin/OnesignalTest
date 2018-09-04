05894fbfe44c62439af6f5432f9feea1790c4e0d
duke <duke@chope.co>
2018-09-04 10:32:10 +0800

    finish master_featuresSetup_ActionButton_BackgroundNotifications

--- ChopeOneSignal/AppDelegate.m
+++ ChopeOneSignal/AppDelegate.m
@@ -28,6 +28,26 @@
 
     id notificationReceiverBlock = ^(OSNotification *notification) {
         NSLog(@"Received Notification - %@", notification.payload.notificationID);
+        
+        // 接受silent-notifications
+        // 静默消息注意三点： 发送背景通知
+//        1. Notification Service Extension. See Step 1 of our iOS Native SDK setup guide to add one to your Xcode project. This will only fire for iOS 10+.
+//        2. Add a notification message or API contents parameter.
+        // 不能添加content title 以及 subtitle等。不然通知就是可见的
+//        3. Enabling Mutable Content in the Dashboard > New Message > Options section or the API mutable_content parameter
+        
+        // 发送无声通知
+//        静音通知专为原生iOS和Android应用而设计。您可以使用本机代码在非本机应用程序上实现它们。
+//
+//        iOS - 只需启用content_available = true
+//        这将允许您省略消息正文以防止显示通知，以便您可以发送所需的任何数据。
+
+//        iOS静音通知仅在应用程序处于打开状态且在后台或前台时才有效。
+//        如果应用程序已强制退出或“刷掉”，您可以通过向通知添加内容来“唤醒”。
+
+
+        
+
     };
     
     id notificationOpenedBlock = ^(OSNotificationOpenedResult *result) {
